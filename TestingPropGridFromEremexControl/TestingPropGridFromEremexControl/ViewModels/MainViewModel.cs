a745b12dcc6a8afbf34d5c0bd691db0e19a0c680
Fedor <asa193@mail.ru>
2024-06-03 16:51:06 +0300

    Пример медленной работы AddRange в SelectedNodes

--- TestingPropGridFromEremexControl/TestingPropGridFromEremexControl/ViewModels/MainViewModel.cs
+++ TestingPropGridFromEremexControl/TestingPropGridFromEremexControl/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DynamicData;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -22,125 +24,35 @@ namespace TestingPropGridFromEremexControl.ViewModels {
         [ObservableProperty]
         object[] selectedObjects = [];
 
+        public ObservableRangeCollection<object> Nodes { get; set; } = [];
+
+        [ObservableProperty]
+        private ObservableRangeCollection<object> _selectedNodes = [];
+
         public MainViewModel() {
             List<object> list = new List<object>();
-            while (list.Count < 1000) {
+            while (list.Count < 100000) {
                 list.Add(new ManWithObser() { HaveName = true, Name = "With obs" });
                 //list.Add(new ManWithoutObser() { HaveName = true, Name = "Without obs" });
             }
-            
-            SelectedObjects = list.ToArray();
 
-            //Поток чтобы точно проверить что меняются свойства, не зависимо Observable или нет.
-            Thread b = new Thread(Timer);
-            b.IsBackground = true;
-            b.Start();
+            Nodes.AddRange(list.ToArray());
+
+            SelectedNodes.CollectionChanged += SelectedNodes_CollectionChanged;
         }
 
-        partial void OnTestingNameChanged(string value) {
-            switch (SelectedObjects[0]) {
-                case ManWithObser man:
-                    man.Name = value;
-                    break;
-                case ManWithoutObser manWithout:
-                    manWithout.Name = value;
-                    break;
-            }
+        private void SelectedNodes_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e) {
         }
 
-        private void Timer() {
-            while(true) {
-                switch (SelectedObjects[0]) {
-                    case ManWithObser man:
-                        CurrentName = man.Name;
-                        CurrentReadOnly = man.HaveName;
-                        break;
-                    case ManWithoutObser manWithout:
-                        CurrentName = manWithout.Name;
-                        CurrentReadOnly = manWithout.HaveName;
-                        break;
-                }
-                System.Threading.Thread.Sleep(100);
-            }
+        public void SelectAllWithNewCol() {
+            SelectedNodes = new ObservableRangeCollection<object>(Nodes);
         }
 
-        public IEnumerable MyRowSource { get => GetMyRowSource(); }
+        public void SelectAllWithAddRange() {
 
-        public IEnumerable GetMyRowSource() {
-            if (SelectedObjects.Length == 0)
-                return new List<object>();
-
-            var firstObject = SelectedObjects.First();
-            List<PropertyInfo> myProperties = [.. firstObject.GetType().GetProperties()];
-
-            foreach (var o in SelectedObjects.Skip(1)) {
-                var p = o.GetType().GetProperties();
-                myProperties = myProperties.IntersectBy(p.Select(n => n.Name), x => x.Name).ToList();
-            }
-
-            var list = new List<object>();
-            foreach (var p in myProperties) {
-                string? DisplayName = p.GetCustomAttributes(typeof(DisplayNameAttribute)).Cast<DisplayNameAttribute>().FirstOrDefault()?.DisplayName;
-                if (DisplayName == null)
-                    DisplayName = p.Name;
-
-                var Allowed = p.GetCustomAttributes(typeof(AllowedByProp)).Cast<AllowedByProp>().FirstOrDefault();
-
-                if(Allowed == null)
-                    list.Add(new DefaultRowViewModel(null, null) { FieldName = p.Name, Caption = DisplayName, ReadOnly = false });
-                else
-                    list.Add(new DefaultRowViewModel(firstObject as ObservableObject, myProperties.Find(p => p.Name == Allowed.PropName)) 
-                    { FieldName = p.Name, Caption = DisplayName });
-            }
-            return list;
+            SelectedNodes.Clear();
+            SelectedNodes.AddRange(Nodes);
         }
     }
 
-    public class CategoryRowViewModel {
-        // The category's display name.
-        public string Caption { get; set; }
-
-        // Child row View Models that will be rendered as child rows.
-        public IEnumerable Items { get; set; }
-    }
-
-    public class DefaultRowViewModel : ObservableObject {
-
-        public DefaultRowViewModel(ObservableObject? model, PropertyInfo? propForAllow) {
-            _model = model;
-            _propForAllow = propForAllow;
-            if(model !=null)
-                model.PropertyChanged += OnPropertyChanged;
-            ReadOnly = !AllowProp();
-        }
-
-        private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e) {
-            if (e.PropertyName == _propForAllow?.Name)
-                ReadOnly = !AllowProp();
-        }
-
-        private bool AllowProp() {
-            if(_model == null) return true;
-            if (_propForAllow?.GetValue(_model) is bool b)
-                return b;
-            else
-                return true;
-        }
-
-        private ObservableObject? _model;
-
-        private PropertyInfo? _propForAllow;
-
-        // The path to a target object's property.
-        public required string FieldName { get; set; }
-
-        // The property's display name.
-        public required string Caption { get; set; }
-
-        private bool enabled;
-        public bool ReadOnly {
-            get => enabled;
-            set => SetProperty(ref enabled, value);
-        }
-    }
 }
